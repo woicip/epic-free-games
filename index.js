8b32061adbd38594e567207f813e563d5a837204
woicip <woicipta@gmail.com>
2023-05-15 20:19:41 +0700

    update: applied caching method for fast response

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
+const { JsonDB, Config } = require('node-json-db');
+const cron = require('node-cron')
+
+// modules
 const GetFreeGames = require('./GetFreeGames');
+
+const config = new Config("efg", true, false, "/")
+const db = new JsonDB(config)
+
+async function getData(){
+    const result = await GetFreeGames()
+    await db.push("efg", result)
+}
+
+cron.schedule('*/30 * * * *', () => {
+    getData()
+})
 
 app.get('/', async (req, res) => {
     try {
-        const games = await GetFreeGames();
+        const games = await db.getData('efg')
         res.send({ code: 200, message: "SUCCESS", games });
 
     } catch(err){
-        console.log(err)
-        res.send({ code: 500, message: "Something went wrong when getting requested resource", games: null });
+        res.status(500).send({ code: 500, message: "Something went wrong when getting requested resource", games: null });
     }
 });
 
-app.listen(3303, () => console.log('Epic Free Games service is running at http://localhost:3303'));
+app.listen(3303, () => {
+    getData()
+    console.log('Epic Free Games service is running at http://localhost:3303')
+});
