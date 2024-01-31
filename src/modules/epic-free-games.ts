a3fc6144a09d718ec2ccaf4328c413883620f4ca
woicip <woicipta@gmail.com>
2024-01-31 14:58:42 +0700

    update: headless setting

--- src/modules/epic-free-games.ts
+++ src/modules/epic-free-games.ts
@@ -11,7 +11,7 @@ function removeTags(str: string) {
 }
 
 export async function EpicFreeGames(){
-    const browser = await playwright.chromium.launch({ headless: false });
+    const browser = await playwright.chromium.launch({ headless: true });
     const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
     });
